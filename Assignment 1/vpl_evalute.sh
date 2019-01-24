34ed9e9ada5d026d385a6aa03cdcfa7f5161e5e0
Shadab Zafar <dufferzafar0@gmail.com>
2019-01-24 18:07:10 +0530

    diff with old or new

--- Assignment 1/vpl_evalute.sh
+++ Assignment 1/vpl_evalute.sh
@@ -40,14 +40,40 @@ grade=0
 
 # Model files are already present!
 tar --overwrite -xf model.tar
+tar --overwrite -xf model.old.tar
 
 # Match all parts
 for part in $(seq 1 25); do
-    diff -qwB "part-$part.out" "part-$part.model" > /dev/null 2>&1
-    error=$?
-    if [ $error -eq 0 ]; then
-        grade=$((grade+4))
+
+    # Part 24 is handled separately
+    if [ $part -eq 24 ]; then
+
+        # Some students have printed journals with 0 impact factor
+        # While others have not, so we only check one row
+        # ijet journal should have 0.125 impact factor
+        part24=$(grep "ijet" "part-$part.out" | cut -d"|" -f 2 | grep -c "0\.125")
+        if [ $part24 -eq 1 ]; then
+            grade=$((grade+2))
+        else
+            echo "Part $part is wrong."
+        fi
+
+    else
+
+        diff -qwB "part-$part.out" "part-$part.model" > /dev/null 2>&1
+        error1=$?
+
+        diff -qwB "part-$part.out" "part-$part.old.model" > /dev/null 2>&1
+        error2=$?
+
+        if [ $error1 -eq 0 || $error2 -eq 0 ]; then
+            grade=$((grade+2))
+        else
+            echo "Part $part is wrong."
+        fi
+
     fi
+
 done
 
 set_grade $grade;
