aff8de015f34560bcd640265633bf701b24f78b8
Harish Thuwal <hthuwal@gmail.com>
2019-02-04 00:17:26 +0530

    Mass Mailer

---
+++ Project 1/massmailer/mass_mail.py
@@ -0,0 +1,45 @@
+import csv
+import smtplib
+import sys
+from config import *
+
+# Dirty Hack
+sys.path.append("../datadrop")
+from credentials import CREDENTIALS
+
+
+SUBJECT = "DBMS Project 1 Credentials"
+
+
+def setup_connection(sender):
+    """
+    Establish a connection with the smtp server
+    """
+
+    smtp = smtplib.SMTP(SMTP_SERVER)
+    # smtp.set_debuglevel(True)
+    user = sender["uname"]
+    password = sender["pswd"]
+    smtp.starttls()
+    smtp.login(user, password)
+    return smtp
+
+
+if __name__ == "__main__":
+    conn = setup_connection(SENDER)
+
+    with open(RECIPIENTS_FILE) as recepients_file:
+        reader = csv.reader(recepients_file)
+        for row in reader:
+            group = row[0]
+            tolist = row[1:]
+
+            if group not in CREDENTIALS:
+                print("Credentials for {group} does not exist.".format(group=group))
+            else:
+                print("Sending Credentials to {}".format(", ".join(tolist)))
+
+                msg = EMAIL_TEMPLATE.format(uname=group, pswd=CREDENTIALS[group])
+                msg = "Subject: {}\n\n{}".format(SUBJECT, msg)
+
+                conn.sendmail(SENDER["email"], tolist, msg)
