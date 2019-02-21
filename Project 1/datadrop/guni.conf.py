25497a1eab89a7cbd189922744d25ee1fa6a4ceb
Shadab Zafar <dufferzafar0@gmail.com>
2019-02-21 19:25:28 +0530

    Update gunicorn.conf

--- Project 1/datadrop/guni.conf.py
+++ Project 1/datadrop/guni.conf.py
@@ -1,11 +1,13 @@
 bind = '0.0.0.0:5000'
-backlog = 512
+backlog = 2048
 
 workers = 17
 worker_class = 'sync'
 worker_connections = 1000
-timeout = 30
+timeout = 100
 keepalive = 2
+
+reload = True
 
 daemon = True
 pidfile = "/tmp/gunicorn.pid"
