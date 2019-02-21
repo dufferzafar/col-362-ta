c3995b73eafb1576e64dd51fb6e6dad53b17d045
Shadab Zafar <dufferzafar0@gmail.com>
2019-02-21 18:34:19 +0530

    Gunicorn Config file

---
+++ Project 1/datadrop/guni.conf.py
@@ -0,0 +1,20 @@
+bind = '0.0.0.0:5000'
+backlog = 512
+
+workers = 17
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+daemon = True
+pidfile = "/tmp/gunicorn.pid"
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+errorlog = 'gunicorn.error.log'
+loglevel = 'info'
+accesslog = 'gunicorn.access.log'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
